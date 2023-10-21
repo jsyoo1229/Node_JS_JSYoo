c2ecae618225615f2f0496ed8b521e393b6ceb96
Jinsun Yoo <jsyoo1029@gmail.com>
2023-10-22 04:54:24 +0900

    게시판 리스트 & 글쓰기 UI

---
+++ Board/app.js
@@ -0,0 +1,18 @@
+const express= require ("express");
+const handlebars= require("express-handlebars");
+const app= express();
+
+app.engine("handlebars", handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname+ '/views');
+
+app.get("/", (req, res)=>{
+    res.render('home', {title: '게시판', message: '만나서 반갑습니다'});
+});
+
+app.get("/write", (req, res)=>{
+    res.render('write', {title: '게시판'});
+});
+
+app.listen(3000);
+
